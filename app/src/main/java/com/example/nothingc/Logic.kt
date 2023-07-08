ebeb99f58a4b2f10f96a4ed7593009dce1d0d93e
Rene Hippe <rene.hippe@study.hs-duesseldorf.de>
2023-07-09 01:35:48 +0200

    add some operations

--- app/src/main/java/com/example/nothingc/Logic.kt
+++ app/src/main/java/com/example/nothingc/Logic.kt
@@ -7,19 +7,57 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.cos
+import kotlin.math.log
+import kotlin.math.log10
+import kotlin.math.log2
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.math.tan
 
 
-private fun Add(){}
-private fun Subtract(){}
-private fun Multiply(){}
-private fun Divide(){}
-private fun Power(){}
-private fun Percentage(){}
-private fun Root(){}
-private fun Sin(){}
-private fun Cos(){}
-private fun Tan(){}
-private fun Cosin(){}
+fun Add(num1: Double, num2: Double) : Double{
+    return num1 + num2
+}
+fun Subtract(num1: Double, num2: Double) : Double{
+    return num1 - num2
+}
+fun Multiply(num1: Double, num2: Double) : Double{
+    return num1 * num2
+}
+fun Divide(num1: Double, num2: Double) : Double{
+    return num1 / num2
+}
+fun Power(num1: Double, num2: Double) : Double{
+    return num1.pow(num2)
+}
+fun Percentage(num1: Double, num2: Double) : Double{
+    return (num2/100)*num1
+}
+fun Root(num1: Double) : Double{
+    return sqrt(num1)
+}
+fun Sin(num1: Double) : Double{
+    return sin(num1)
+}
+fun Cos(num1: Double) : Double{
+    return cos(num1)
+}
+fun Tan(num1: Double) : Double{
+    return tan(num1)
+}
+fun Log10(num1: Double) : Double{
+    return log10(num1)
+}
+
+fun Log2(num1: Double) : Double{
+    return log2(num1)
+}
+
+fun Log(num1: Double, num2: Double) : Double{
+    return log(num1, num2)
+}
 
 
 @Composable
