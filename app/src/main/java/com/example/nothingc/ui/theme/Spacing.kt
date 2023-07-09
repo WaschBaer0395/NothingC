dd7514334c26c005d16c77ed421aa62005898435
Rene Hippe <rene.hippe@study.hs-duesseldorf.de>
2023-07-09 09:47:11 +0200

    removed .dp

--- app/src/main/java/com/example/nothingc/ui/theme/Spacing.kt
+++ app/src/main/java/com/example/nothingc/ui/theme/Spacing.kt
@@ -2,18 +2,19 @@ package com.example.nothingc.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.unit.dp
+import com.example.nothingc.scaledDp
 
 object Spacing {
-    val xxxs = 2.dp
-    val xxs = 4.dp
-    val xs = 8.dp
-    val st = 12.dp
-    val sm = 16.dp
-    val md = 24.dp
-    val lg = 32.dp
-    val xl = 48.dp
-    val xxl = 64.dp
-    val xxxl = 80.dp
+    val xxxs = 2
+    val xxs = 4
+    val xs = 8
+    val st = 12
+    val sm = 16
+    val md = 24
+    val lg = 32
+    val xl = 48
+    val xxl = 64
+    val xxxl = 80
 }
 
 val MaterialTheme.spacing : Spacing get() = Spacing
